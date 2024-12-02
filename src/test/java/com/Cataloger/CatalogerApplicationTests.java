0a185e6ed55d3febe234fbf11886ad40c8809b09
Ayoub CHAABAT <chaabat.02@gmail.com>
2024-12-02 09:46:36 +0100

    spring boot project

---
+++ src/test/java/com/Cataloger/CatalogerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cataloger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
